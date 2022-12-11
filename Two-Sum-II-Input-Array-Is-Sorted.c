c392653637bf16cc0399b47ee864a0b30cda8f8b
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-11 22:00:14 +0530

    11-

---
+++ Two-Sum-II-Input-Array-Is-Sorted.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+ 
+             
+                for(  int k=0; k<numsSize; k++ ) {
+                    for( int s=k+1; s<numsSize; s++) {
+                        if (nums[k] + nums[s] == target) {
+                            int * sai = malloc(2 * sizeof(int));
+                            *returnSize = 2;
+                      sai[0] = k;
+                      sai[1] = s;
+                            return sai;
+                        }       
+                    }
+                 }
+         
+  return NULL;
+}
