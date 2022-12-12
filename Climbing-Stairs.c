66ad878547c869c2565a32b54929b2f86f563482
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-12 21:31:54 +0530

    12-DEC-2022

--- Climbing-Stairs.c
+++ Climbing-Stairs.c
@@ -2,6 +2,25 @@
 
 int climbStairs(int n){
          
+        
+                int n1=0, n2=1,n3;
+        for( int k=0;k<n;k++) {
+            n3 = n1+n2;
+            n1 = n2;
+            n2 = n3;
+
+        }
+        
+        return n3;
+        
+}
+
+
+-------------------------- OR --------------------------
+
+
+int climbStairs(int n){
+         
         if( n == 1)
             return 1;
             else {
@@ -17,3 +36,6 @@ int climbStairs(int n){
             }
             return NULL;
 }
+
+
+
