7add5707e79826b53fe2516c7889f7196816fa7b
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-15 21:23:32 +0530

    15-DEC-2022

--- Search-Insert-Position.c
+++ Search-Insert-Position.c
@@ -1,11 +1,9 @@
  int searchInsert(int* nums, int numsSize, int target){
       
-
      for(int k=0;k<numsSize;k++ ) {
-         if(  nums[k] >= target) {
-              return k;
-             
-         }
+         if(  nums[k] >= target)  
+              return k;   
+         
      }           return numsSize;
 
  }
