79e389bbdcde64fb6d77b99ddd63f7b9849629f7
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-24 21:04:39 +0530

    15-12-2022

--- Search-Insert-Position.c
+++ Search-Insert-Position.c
@@ -1,9 +1,24 @@
- int searchInsert(int* nums, int numsSize, int target){
-      
-     for(int k=0;k<numsSize;k++ ) {
-         if(  nums[k] >= target)  
-              return k;   
-         
-     }           return numsSize;
+https://leetcode.com/problems/search-insert-position/ 
 
- }
+int searchInsert(int* nums, int numsSize, int target){
+
+      int start = 0;
+      int end = numsSize - 1;
+
+      while( start <= end ) {
+          int mid = start + (end - start );
+
+          if( nums[mid] > target) {
+              end = mid - 1;
+          } 
+          else if ( nums[mid] < target ) {
+              start = mid + 1;
+
+          }
+          else 
+            {
+                return mid;
+            }
+      }
+      return start; 
+      }
